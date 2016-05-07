36c8c200a5b8de783b9cc33e686bcb5aed5a4841
陈新乐 <chenxinle@chenxinledeMacBook-Pro.local>
2016-05-07 18:12:18 +0800

    gulp init

--- angularHelloWorld/todo.js
+++ angularHelloWorld/todo.js
@@ -1,4 +1,21 @@
 angular.module('app', [])
 	.controller('todoCtrl', function($scope) {
-		$scope.totalTodos = 4;
+		$scope.todoList = [
+			{text: 'learn angularJs', done: false}, 
+			{text: 'build app', done: false}, 
+			{text: 'learn gulp', done: false}
+		];
+		$scope.getTotalTodos = function() {
+			return $scope.todoList.length;
+		};
+		$scope.addToList = function() {
+			$scope.todoList.push({text: $scope.textNew, done:false});
+		}
+		$scope.removeCompleted = function() {
+			var oldList = $scope.todoList;
+			$scope.todoList = [];
+			angular.forEach(oldList, function(todo){
+				if (!todo.done) $scope.todoList.push(todo);
+			});
+		};
 	})
